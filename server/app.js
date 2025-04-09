86396fa0099d832242409ab353d43f9957c99eab
Zee Khoo <zeekhoo@amaterasu2.local>
2025-04-08 21:37:29 -0700

    add server

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+var bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express()
+const port = 3000
+app.use(bodyParser.json())
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/api/cold-harbor', (req, res) => {
+  const data = req.body
+  console.log(data)
+  res.send(`Received data: ${JSON.stringify(data)}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- src/views/ColdHarborView.vue
+++ src/views/ColdHarborView.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
-    <div class="about">
-      <h1>Cold Harbor</h1>
+    <h1 class="">Cold Harbor</h1>
+    <div class="inputarea">
+      <p>Enter your refinement data:</p>
+      <div>
+        <input class="textarea" v-model="macrodata" placeholder="The work is mysterious and important..." />
+      </div>
+      <div>
+        <button 
+          class="submit-button"
+          @click="submit">Submit</button>
+      </div>
     </div>
     <div class="back-button">
       <RouterLink to="/">Back</RouterLink>
     </div>
   </div>  
 </template>
+
+<script setup>
+const macrodata = defineModel();
+
+async function submit(event) {
+  const value = macrodata.value;
+  res =await fetch('http://localhost:3000/api/cold-harbor', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      data: value
+    })
+  });
+}
+</script>
 
 <style scoped>
 .about {
@@ -18,7 +44,44 @@
 }
 .back-button {
   text-align: center;
+  margin-top: 120px;
   margin-bottom: 60px;
   align-items: bottom;
+}
+.submit-button {
+  background-color: #04AA6D; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 20px;
+  border-radius: 4px;
+  transition-duration: 0.4s;
+}
+.submit-button:hover {
+  background-color: white;
+  color: #04AA6D;
+  cursor: pointer;
+}
+.submit-button:active {
+  background-color: #3e8e41;
+  transform: translateY(4px);
+}
+.inputarea {
+  width: 600px;
+  text-align: top;
+}
+.textarea {
+  width: 100%;
+  height: 150px;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  resize: none;
 }
 </style>
